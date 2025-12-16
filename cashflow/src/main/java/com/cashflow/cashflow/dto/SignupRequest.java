1d4ffb8bbf3a12472eb068f9da1185fbec1f09ee
Harshvardhan Tiwari <harshvardhanti12@gmail.com>
2025-12-16 20:01:58 +0530

    Add JSON format annotation to dob field

--- cashflow/src/main/java/com/cashflow/cashflow/dto/SignupRequest.java
+++ cashflow/src/main/java/com/cashflow/cashflow/dto/SignupRequest.java
@@ -1,17 +1,27 @@
 package com.cashflow.cashflow.dto;
 
-import jakarta.persistence.Entity;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 public record SignupRequest(
+
         @NotBlank String name,
-        @NotBlank @Email String email,
-        @NotBlank @Size(min = 4) String username,
-        @NotBlank @Size(min = 6) String password,
-        String dob
+
+        @NotBlank
+        @Email String email,
+
+        @NotBlank
+        @Size(min = 4)
+        String username,
+
+        @NotBlank
+        @Size(min = 6)
+        String password,
+
+        @JsonFormat(pattern = "yyyy-MM-dd")
+        LocalDate dob        // ✅ FIXED
 ) {}
